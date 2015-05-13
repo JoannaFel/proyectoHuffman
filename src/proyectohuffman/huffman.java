b270712126af0994bad5a7fa619e991a24154d48
florez666 <oscarflorez89@gmail.com>
2015-05-12 19:37:02 -0500

    metodo para contar las letras del txt (incompleto)

--- src/proyectohuffman/huffman.java
+++ src/proyectohuffman/huffman.java
@@ -79,6 +79,25 @@ public class huffman {
         }        
     }
      
+    public void contarCaracteres(){
+        String textoleido ="";
+        
+        char[] letras = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n'
+                ,'o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4'
+                ,'5','6','7','8','9','0','.',',',' '};
+        
+        //int[] cantXletra = new int[];
+        
+        //textoleido = texto.leerArchivo();
+        
+        //convertimos el string a una cadena de caracteres
+        char[] textoLeidoChar = textoleido.toCharArray();
+        int j=0;
+        for(int i=0; i <= textoLeidoChar.length; i++){
+            //if(textoLeidoChar[i] == letras[j] || letras[j+1] || letras[j+2])
+              // j++ 
+        }
+    }
     
     public void gestionar(){
         crearListaNodos();
